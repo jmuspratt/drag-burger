30c36f075281576d7b0d875ed7a45452715e49d3
James Muspratt <james@jamesmuspratt.com>
2020-04-23 11:28:36 -0400

    Clean up

--- burger.js
+++ burger.js
@@ -1,9 +1,6 @@
-
-
-
 class DragBurger {
     constructor() {
-        const button = document.querySelector(".button");
+        const button = document.querySelector('.button');
         this.$wrap = $('.wrap');
         this.$button = $('.button');
         this.$main = $('.main');
@@ -13,8 +10,8 @@ class DragBurger {
         this.menuIsOpen = false;
 
         // Always remove snapping class after transition ends
-        (this.$button, this.$main).on("transitionend", () => {
-            this.$wrap.removeClass("snapping");
+        (this.$button, this.$main).on('transitionend', () => {
+            this.$wrap.removeClass('snapping');
         });
 
         // create a simple instance
@@ -28,52 +25,37 @@ class DragBurger {
         });
 
         // Drag
-        mc.on("pan", (event) => {
-            // console.log(event.deltaX);
-            // console.log(event);
-
+        mc.on('pan', (event) => {
             const moveMain =  1.2 * event.deltaX;
             const moveButton =  event.deltaX;
 
             const mainPosX = moveMain + this.savedMainX <= 0 ? moveMain + this.savedMainX : 0; // don't let it go right of origin
             const buttonPosX = moveButton + this.savedButtonX;
 
-            $(".main").css('transform', `translateX(${mainPosX}px)`);
-            $(".button").css('transform', `translateX(${buttonPosX}px)`);
+            $('.main').css('transform', `translateX(${mainPosX}px)`);
+            $('.button').css('transform', `translateX(${buttonPosX}px)`);
 
-            // release
+            // Release drag
             if (event.isFinal) {
-            // buttonIsDragging = false;
-            
-            // console.log('window is ', windowWidth);
-            const windowWidth = document.documentElement.clientWidth;
-            const snapOpenMark = - windowWidth / 2;
-            const snapOpen = event.deltaX < snapOpenMark;
-            
-            this.snap(snapOpen, windowWidth);
-
-            // console.log('SNAPPING');
-            // console.log('delta', event.deltaX);
-            // console.log('snapOpenMark', snapOpenMark);
-
-
-            // console.log('saved button x', savedButtonX);
-            // console.log('saved main x', savedMainX);
-
+                const windowWidth = document.documentElement.clientWidth;
+                const snapOpenMark = - windowWidth / 2;
+                const snapOpen = event.deltaX < snapOpenMark;
+                
+                this.snap(snapOpen, windowWidth);
             }
         });
     }
 
     snap(snapOpen, windowWidth) {
 
-        this.$wrap.addClass("snapping");
+        this.$wrap.addClass('snapping');
             
         if (snapOpen) {
             console.log('snap open');
             const buttonDestinationX = -(windowWidth - 80);
             const mainDestinationX = -(windowWidth - 30);
-            $(".button").css("transform", 'translateX(' + buttonDestinationX + 'px)');
-            $(".main").css("transform", 'translateX(' + mainDestinationX + 'px)');
+            $('.button').css('transform', 'translateX(' + buttonDestinationX + 'px)');
+            $('.main').css('transform', 'translateX(' + mainDestinationX + 'px)');
             this.savedButtonX = buttonDestinationX;
             this.savedMainX = mainDestinationX;
             this.menuIsOpen = true;
@@ -81,8 +63,8 @@ class DragBurger {
         // snapClosed
         else {
             console.log('snap closed');
-            $(".button").css("transform", "translateX(" + 0 + "px)");
-            $(".main").css("transform", "translateX(" + 0 + "px)");
+            $('.button').css('transform', 'translateX(' + 0 + 'px)');
+            $('.main').css('transform', 'translateX(' + 0 + 'px)');
             this.savedButtonX = 0;
             this.savedMainX = 0;
             this.menuIsOpen = true;
